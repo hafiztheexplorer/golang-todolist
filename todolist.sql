a1f27afdef97598dec1589e244da5e442ad3eeb7
Hafiz Nur <hafiznur@live.com>
2023-09-26 17:09:57 +0700

    Initial commit

---
+++ todolist.sql
@@ -0,0 +1,39 @@
+SHOW DATABASES;
+SHOW CREATE TABLE todolist;
+use gorestfulapi_exercise;
+select * from todolist;
+desc todolist;
+
+rename table `todolistcontent` to `todolist`;
+alter table `todolist` rename column `namatodolist` to `todolisttitle`;
+alter table `todolist` add column `todolistcontent` text after `todolisttitle`;
+alter table `todolist` add column `todolisttabledate` DATETIME after `id`;
+alter table `todolist` add column `todolistcontentchecked` bool after `todolistcontent`;
+alter table `todolist` rename column `todolistcontentchecked` to `checked`;
+alter table `todolist` add column `todolistsubcontent` text after `todolistcontent`;
+alter table `todolist` MODIFY column `checked` boolean; 
+alter table `todolist` modify column `datetime` timestamp not null;
+
+delete from todolist where id="";
+insert into `todolist` (`todolisttitle`, `todolistcontent`, `todolistsubcontent`, `checked`)
+values
+('to do list 1', 'contoh isi main to do list 2', 'sub kategori to do list', true),
+('to do list 2', 'contoh isi main to do list 1', 'sub kategori to do list', true),
+('to do list 2', 'contoh isi main to do list 2', 'sub kategori to do list', true),
+('to do list 3', 'contoh isi main to do list 1', 'sub kategori to do list', true);
+
+alter table `todolist` drop COLUMN `todolistsubcontent`;
+
+/*-------------------------------------------------------------------------------------*/
+create database `db_todolist`;
+use `db_todolist`;
+CREATE TABLE `todolist` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `datetime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `todolisttitle` varchar(255) NOT NULL,
+  `todolistcontent` text,
+  `checked` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+select * from todolist;
+desc todolist;
